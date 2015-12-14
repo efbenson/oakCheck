b689f8ec77df1f4e281c4a58212d8e353faa5df2
corporate\ebenson <ebenson@gfnet.com>
2015-12-14 01:18:28 -0500

    Adds unit tests for $or

--- test/base_test.js
+++ test/base_test.js
@@ -23,7 +23,6 @@ describe('oak-check', function () {
                     done('failed should have passed')        
                 };
             });
-
             it('should fail eq', function (done) {
                 var processor = new Processor();
 
@@ -33,6 +32,84 @@ describe('oak-check', function () {
                     done('passed should have failed')        
                 };
             });
+            it('should pass nested eq', function (done) {
+                var processor = new Processor();
+
+                if (processor.check(
+                    {'$eq' :
+                        {'robots.johnny': 5}},
+                    {robots :
+                        {'johnny': 5}}
+                    )) {
+                    done()
+                }else{
+                    done('passed should have passed');
+                };
+            });
+            it('should fail nested eq', function (done) {
+                var processor = new Processor();
+
+                if (!processor.check(
+                    {'$eq' :
+                        {'robots.johnny': 5}},
+                    {robots :
+                        {'johnny': 4}}
+                    )) {
+                    done()
+                }else{
+                    done('passed should have passed');
+                };
+            });
+        });
+        describe('$or', function(){
+            it('should pass or eq', function (done) {
+                var processor = new Processor();
+
+                if (processor.check({'$or' :
+                            [{'$eq' : {'johnny': 5}},
+                            {'$eq' : {'johnny': 6}}]},
+                        {'johnny': 6})) {
+                    done()
+                }else{
+                    done('failed should have passed');
+                };
+            });
+            it('should fail or eq', function (done) {
+                var processor = new Processor();
+
+                if (!processor.check({'$or' :
+                            [{'$eq' : {'johnny': 5}},
+                            {'$eq' : {'johnny': 6}}]},
+                        {'johnny': 7})) {
+                    done()
+                }else{
+                    done('failed should have passed');
+                };
+            });
+            it('should pass nested or eq', function (done) {
+                var processor = new Processor();
+
+                if (processor.check({'$or' :
+                            [{'$eq' : {'robots.johnny': 5}},
+                            {'$eq' : {'robots.johnny': 6}}]},
+                         {robots : {'johnny': 6}})) {
+                    done()
+                }else{
+                    done('failed should have passed');
+                };
+            });
+            it('should fail nested or eq', function (done) {
+                var processor = new Processor();
+
+                if (!processor.check({'$or' :
+                            [{'$eq' : {'robots.johnny': 5}},
+                            {'$eq' : {'robots.johnny': 6}}]},
+                         {robots : {'johnny': 4}})) {
+                    done()
+                }else{
+                    done('failed should have passed');
+                };
+            });            
         });
     });
 });
